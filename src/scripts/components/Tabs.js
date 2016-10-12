3f181e7fb8a8d4d94ab0ed8dc631404debbc47c3
peter <peter.shenlq@gmail.com>
2016-10-12 16:22:39 +0800

    improve: 设置Tabs.Item默认被选中颜色

--- src/scripts/components/Tabs.js
+++ src/scripts/components/Tabs.js
@@ -1,3 +1,6 @@
 import TabNavigator from 'react-native-tab-navigator';
+import { textPrimary } from '../../styles/utils';
+
+TabNavigator.Item.defaultProps.selectedTitleStyle = textPrimary;
 
 export default TabNavigator;
