d1155fdfd58d62349eb5a0d1a36ff0c504895e6b
peter <peter.shenlq@gmail.com>
2016-10-18 11:10:49 +0800

    fix: StausBar在安卓下没有高度

--- src/scripts/components/StatusBar.js
+++ src/scripts/components/StatusBar.js
@@ -3,16 +3,28 @@ import { View, StatusBar } from 'react-native';
 import { statusBar } from '../../styles/modules/statusBar';
 
 // StatusBarWrap
-const StatusBarWrap = ({ style }) => {
+const StatusBarWrap = ({ style, animated, showHideTransition }) => {
     return (
         <View style={[style, statusBar]}>
-            <StatusBar barStyle="light-content" />
+            <StatusBar
+                barStyle="light-content"
+                translucent
+                animated={animated}
+                showHideTransition={showHideTransition}
+                backgroundColor={statusBar.backgroundColor} />
         </View>
     );
 };
 
 StatusBarWrap.propTypes = {
+    animated: PropTypes.bool,
+    showHideTransition: PropTypes.oneOf(['fade', 'slide']),
     style: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
+};
+
+StatusBarWrap.defaultProps = {
+    showHideTransition: 'fade',
+    animated: false,
 };
 
 export default StatusBarWrap;
